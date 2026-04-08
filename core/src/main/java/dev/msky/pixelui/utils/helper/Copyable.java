aa2c6f07844f36928d487fde12601ae96105d2a2
Markus <markus.slivovsky89@gmail.com>
2026-04-08 19:51:30 +0200

    copable helper added

--- core/src/main/java/dev/msky/pixelui/utils/helper/Copyable.java
+++ core/src/main/java/dev/msky/pixelui/utils/helper/Copyable.java
@@ -1,5 +1,16 @@
 package dev.msky.pixelui.utils.helper;
 
+import com.badlogic.gdx.utils.Array;
+
 public interface Copyable<T> {
-    T copy();
+    Copyable<T> copy();
+
+    static Array<Copyable> copyArray(Array<Copyable> array){
+        Array<Copyable> copy = new Array<>();
+        for(int i = 0; i < array.size; i++){
+            copy.add(array.get(i).copy());
+        }
+        return array;
+    }
+
 }
