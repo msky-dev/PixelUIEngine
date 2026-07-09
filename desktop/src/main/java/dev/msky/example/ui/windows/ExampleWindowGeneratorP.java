f9726d9e964993e7f9a40fe9184f571625bc368d
Markus <markus.slivovsky89@gmail.com>
2026-07-09 16:16:11 +0200

    german umlauts added to font

--- desktop/src/main/java/dev/msky/example/ui/windows/ExampleWindowGeneratorP.java
+++ desktop/src/main/java/dev/msky/example/ui/windows/ExampleWindowGeneratorP.java
@@ -116,7 +116,7 @@ public class ExampleWindowGeneratorP implements WindowGenerator.P1<MediaManager>
 
         Text text1 = api.component.text.create(1, 5, 0, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         Text text2 = api.component.text.create(1, 4, 0, "abcdefghijklmnopqrstuvwxyz");
-        Text text3 = api.component.text.create(1, 3, 0, "0123456789");
+        Text text3 = api.component.text.create(1, 3, 0, "0123456789 ÄÖÜ äöü ß Wäre würdeßt du?");
         Text text4 = api.component.text.create(1, 2, 0, "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~€¥£©§");
         Text text5 = api.component.text.create(1, 1, 0, "T");
 
