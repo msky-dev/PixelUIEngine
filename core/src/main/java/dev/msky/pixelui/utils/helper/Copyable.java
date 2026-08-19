2d83ba28e3d3a245fc023cab5b4b8312001efa54
Markus <markus.slivovsky89@gmail.com>
2026-08-19 10:03:44 +0200

    copyable interface

--- core/src/main/java/dev/msky/pixelui/utils/helper/Copyable.java
+++ core/src/main/java/dev/msky/pixelui/utils/helper/Copyable.java
@@ -2,89 +2,162 @@ package dev.msky.pixelui.utils.helper;
 
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.IntMap;
-import com.badlogic.gdx.utils.ObjectLongMap;
+import com.badlogic.gdx.utils.LongMap;
+import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.ObjectSet;
 
 import java.util.Arrays;
-import java.util.Stack;
 
 public interface Copyable<T> {
+
     T copy();
 
+    // -------------------------------------------------------------------------
+    // Null safe
+    // -------------------------------------------------------------------------
 
-    static <D extends Copyable<D>> D copyNullSafe(D from){
-        return from != null ? from.copy() : null;
+    static <D extends Copyable<D>> D copyNullSafe(D value) {
+        return value != null ? value.copy() : null;
     }
 
-    static <D extends Copyable<D>> Array<D> copyCollection(Array<D> array) {
-        Array<D> copy = new Array<>(array.size);
-        for (int i = 0; i < array.size; i++) {
-            copy.add(array.get(i).copy());
+    // -------------------------------------------------------------------------
+    // Array
+    // -------------------------------------------------------------------------
+
+    static <D extends Copyable<D>> Array<D> copyCollection(Array<D> source) {
+        Array<D> copy = new Array<>(source.size);
+
+        for (int i = 0; i < source.size; i++) {
+            copy.add(copyNullSafe(source.get(i)));
         }
+
         return copy;
     }
 
-    static <D extends Copyable<D>> ObjectSet<D> copyCollection(ObjectSet<D> objectSet) {
-        ObjectSet<D> copy = new ObjectSet<>();
-        ObjectSet.ObjectSetIterator<D> iterator = objectSet.iterator();
-        while (iterator.hasNext)
-            copy.add(iterator.next());
-        return copy;
-    }
+    // -------------------------------------------------------------------------
+    // ObjectSet
+    // -------------------------------------------------------------------------
 
-    static <D extends Copyable<D>> ObjectLongMap<D> copyCollection(ObjectLongMap<D> objectLongMap) {
-        ObjectLongMap<D> copy = new ObjectLongMap<>();
-        ObjectLongMap.Keys<D> keys = objectLongMap.keys();
-        while (keys.hasNext()){
-            final D key = keys.next();
-            copy.put(key.copy(), objectLongMap.get(key,0));
+    static <D extends Copyable<D>> ObjectSet<D> copyCollection(ObjectSet<D> source) {
+        ObjectSet<D> copy = new ObjectSet<>(source.size);
+
+        ObjectSet.ObjectSetIterator<D> iterator = source.iterator();
+
+        while (iterator.hasNext) {
+            copy.add(copyNullSafe(iterator.next()));
         }
+
         return copy;
     }
 
-    static <D extends Copyable<D>> IntMap<D> copyCollection(IntMap<D> intMap) {
-        IntMap<D> copy = new IntMap<>();
-        IntMap.Keys keys = intMap.keys();
-        while (keys.hasNext){
-            final int key = keys.next();
-            final D value = intMap.get(key);
-            copy.put(key, value != null ? value.copy() : null);
+    // -------------------------------------------------------------------------
+    // ObjectMap
+    //
+    // Keys are NOT copied.
+    // This is especially useful for enum keys:
+    // ObjectMap<MyEnum, MyCopyable>
+    // -------------------------------------------------------------------------
+
+    static <K, D extends Copyable<D>> ObjectMap<K, D> copyCollection(ObjectMap<K, D> source) {
+        ObjectMap<K, D> copy = new ObjectMap<>(source.size);
+
+        for (ObjectMap.Entry<K, D> entry : source) {
+            copy.put(entry.key, copyNullSafe(entry.value));
         }
+
         return copy;
     }
 
+    // -------------------------------------------------------------------------
+    // IntMap
+    // -------------------------------------------------------------------------
 
-    static <D extends Copyable<D>> D[] copyCollection(D[] array) {
-        D[] copy = Arrays.copyOf(array, array.length);
-        for (int i = 0; i < array.length; i++) {
-            D element = array[i];
-            copy[i] = (element != null) ? element.copy() : null;
+    static <D extends Copyable<D>> IntMap<D> copyCollection(IntMap<D> source) {
+        IntMap<D> copy = new IntMap<>(source.size);
+
+        IntMap.Keys keys = source.keys();
+
+        while (keys.hasNext) {
+            int key = keys.next();
+            copy.put(key, copyNullSafe(source.get(key)));
         }
+
         return copy;
     }
 
-    static <D extends Copyable<D>> D[][] copyCollection(D[][] array) {
-        // Copy outer array (keeps runtime type)
-        D[][] copy = Arrays.copyOf(array, array.length);
+    // -------------------------------------------------------------------------
+    // LongMap
+    // -------------------------------------------------------------------------
 
-        for (int i = 0; i < array.length; i++) {
-            D[] inner = array[i];
-            if (inner != null) {
-                // Copy inner array structure
-                D[] innerCopy = Arrays.copyOf(inner, inner.length);
-                // Deep copy elements
-                for (int j = 0; j < inner.length; j++) {
-                    D element = inner[j];
-                    innerCopy[j] = (element != null) ? element.copy() : null;
-                }
-                copy[i] = innerCopy;
-            } else {
+    static <D extends Copyable<D>> LongMap<D> copyCollection(LongMap<D> source) {
+        LongMap<D> copy = new LongMap<>(source.size);
+
+        LongMap.Keys keys = source.keys();
+
+        while (keys.hasNext) {
+            long key = keys.next();
+            copy.put(key, copyNullSafe(source.get(key)));
+        }
+
+        return copy;
+    }
+
+    // -------------------------------------------------------------------------
+    // Arrays
+    // -------------------------------------------------------------------------
+
+    static <D extends Copyable<D>> D[] copyCollection(D[] source) {
+        D[] copy = Arrays.copyOf(source, source.length);
+
+        for (int i = 0; i < source.length; i++) {
+            copy[i] = copyNullSafe(source[i]);
+        }
+
+        return copy;
+    }
+
+    // -------------------------------------------------------------------------
+    // 2D Arrays
+    // -------------------------------------------------------------------------
+
+    static <D extends Copyable<D>> D[][] copyCollection(D[][] source) {
+        D[][] copy = Arrays.copyOf(source, source.length);
+
+        for (int i = 0; i < source.length; i++) {
+            D[] row = source[i];
+
+            if (row == null) {
                 copy[i] = null;
+                continue;
             }
+
+            D[] rowCopy = Arrays.copyOf(row, row.length);
+
+            for (int j = 0; j < row.length; j++) {
+                rowCopy[j] = copyNullSafe(row[j]);
+            }
+
+            copy[i] = rowCopy;
         }
 
         return copy;
     }
 
+    // -------------------------------------------------------------------------
+    // Shallow ObjectSet
+    //
+    // Useful when the contained objects are immutable/non-Copyable.
+    // -------------------------------------------------------------------------
 
+    static <D> ObjectSet<D> copyCollectionShallow(ObjectSet<D> source) {
+        ObjectSet<D> copy = new ObjectSet<>(source.size);
+
+        ObjectSet.ObjectSetIterator<D> iterator = source.iterator();
+
+        while (iterator.hasNext) {
+            copy.add(iterator.next());
+        }
+
+        return copy;
+    }
 }
