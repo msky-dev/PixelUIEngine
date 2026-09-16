f682b1128aafa64940b373b9174f8fd8e132c68d
Markus <markus.slivovsky89@gmail.com>
2026-09-16 20:23:34 +0200

    copyable copy array functions

--- core/src/main/java/dev/msky/pixelui/utils/helper/Copyable.java
+++ core/src/main/java/dev/msky/pixelui/utils/helper/Copyable.java
@@ -143,21 +143,136 @@ public interface Copyable<T> {
         return copy;
     }
 
-    // -------------------------------------------------------------------------
-    // Shallow ObjectSet
-    //
-    // Useful when the contained objects are immutable/non-Copyable.
-    // -------------------------------------------------------------------------
+    static <D extends Copyable<D>> D[] copyArray(D[] array) {
+        D[] copy = Arrays.copyOf(array, array.length);
 
-    static <D> ObjectSet<D> copyCollectionShallow(ObjectSet<D> source) {
-        ObjectSet<D> copy = new ObjectSet<>(source.size);
-
-        ObjectSet.ObjectSetIterator<D> iterator = source.iterator();
-
-        while (iterator.hasNext) {
-            copy.add(iterator.next());
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = copyNullSafe(array[i]);
         }
 
         return copy;
     }
+
+    static <D extends Copyable<D>> D[][] copyArray(D[][] array) {
+        D[][] copy = Arrays.copyOf(array, array.length);
+
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i] != null ? copyArray(array[i]) : null;
+        }
+
+        return copy;
+    }
+
+    static boolean[] copyArray(boolean[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    static boolean[][] copyArray(boolean[][] array) {
+        boolean[][] copy = new boolean[array.length][];
+
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i] != null ? copyArray(array[i]) : null;
+        }
+
+        return copy;
+    }
+
+    static byte[] copyArray(byte[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    static byte[][] copyArray(byte[][] array) {
+        byte[][] copy = new byte[array.length][];
+
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i] != null ? copyArray(array[i]) : null;
+        }
+
+        return copy;
+    }
+
+    static short[] copyArray(short[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    static short[][] copyArray(short[][] array) {
+        short[][] copy = new short[array.length][];
+
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i] != null ? copyArray(array[i]) : null;
+        }
+
+        return copy;
+    }
+
+    static char[] copyArray(char[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    static char[][] copyArray(char[][] array) {
+        char[][] copy = new char[array.length][];
+
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i] != null ? copyArray(array[i]) : null;
+        }
+
+        return copy;
+    }
+
+    static int[] copyArray(int[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    static int[][] copyArray(int[][] array) {
+        int[][] copy = new int[array.length][];
+
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i] != null ? copyArray(array[i]) : null;
+        }
+
+        return copy;
+    }
+
+    static long[] copyArray(long[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    static long[][] copyArray(long[][] array) {
+        long[][] copy = new long[array.length][];
+
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i] != null ? copyArray(array[i]) : null;
+        }
+
+        return copy;
+    }
+
+    static float[] copyArray(float[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    static float[][] copyArray(float[][] array) {
+        float[][] copy = new float[array.length][];
+
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i] != null ? copyArray(array[i]) : null;
+        }
+
+        return copy;
+    }
+
+    static double[] copyArray(double[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    static double[][] copyArray(double[][] array) {
+        double[][] copy = new double[array.length][];
+
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i] != null ? copyArray(array[i]) : null;
+        }
+
+        return copy;
+    }
+
 }
